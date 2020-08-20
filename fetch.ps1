5d23a1a4274395644551acde3dc4b0f570bccca7
yokra9 <53964890+yokra9@users.noreply.github.com>
2020-08-20 21:28:51 +0900

    initial commit

---
+++ fetch.ps1
@@ -0,0 +1,17 @@
+# Step1. Fetch JVN
+2002..(Get-Date).Year | ForEach-Object {
+    docker run --rm -it -v "${PWD}:/vuls" -v "${PWD}\go-cve-dictionary-log:/var/log/vuls" "vuls/go-cve-dictionary" fetchjvn -years $_; 
+}
+
+# Step2. Fetch OVAL for RedHat
+docker run --rm -it -v "${PWD}:/vuls" -v "${PWD}/goval-dictionary-log:/var/log/vuls" "vuls/goval-dictionary" "fetch-redhat" 5 6 7
+
+# Step3. Fetch Go Security Tracker for RedHat
+docker run --rm -i -v "${PWD}:/vuls" -v "${PWD}/gost-log:/var/log/gost" "vuls/gost" fetch redhat
+
+# Step3.5. Fetch go-exploitdb
+docker run --rm -i -v "${PWD}:/vuls" -v "${PWD}/go-exploitdb-log:/var/log/go-exploitdb" "vuls/go-exploitdb" fetch exploitdb
+
+# Step3.6. Fetch go-msfdb
+docker run --rm -i -v "${PWD}:/vuls" -v "${PWD}/go-msfdb-log:/var/log/go-msfdb" "vuls/go-msfdb" fetch msfdb
+
---
+++ init.ps1
@@ -0,0 +1,14 @@
+# Step0. Prepare Log Dir
+mkdir "go-cve-dictionary-log", "goval-dictionary-log", "gost-log", "go-exploitdb-log", "go-msfdb-log"
+
+# Step0.5 Prepare vuln-list repository
+mkdir "vuln-list"
+Set-Location "vuln-list"
+git init
+git config core.protectNTFS false
+git config core.sparsecheckout true
+git config core.longpaths true
+git remote add origin "https://github.com/aquasecurity/vuln-list.git"
+Copy-Item "..\sparse-checkout" ".git\info"
+git pull origin master
+Set-Location "..\"
